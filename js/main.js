92f806d120e9a422ed51c062ae5235267e133661
Bacon <bacon.antife@gmail.com>
2023-03-13 18:52:39 -0300

    Instantaneous results

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ const divCanvas = document.querySelector('.canvas');
 const mapSelect = document.querySelector('#map');
 const canvas = document.createElement('canvas');
 const circlesInput = document.querySelector('#circles');
-const searchersOutput = document.querySelector('#searchers');
+const resultOutput = document.querySelector('#results');
 const ctx = canvas.getContext('2d');
 divCanvas.appendChild(canvas);
 
@@ -17,6 +17,8 @@ let [ projection ] = projections;
 let searchers = [];
 let circles = [];
 let centralizer = new Centralizer({});
+let circleColor = 'rgba(255, 255, 255, 0.5)';
+let searcherColor = '#f70';
 
 projections.forEach((projection, i) => {
 	mapSelect.innerHTML += `<option value="${i}">${projection.name}</option>`
@@ -41,8 +43,8 @@ const drawCircle = (circle) => {
 	const normal = projection.toNormal(circle.coord);
 	const [ x, y ] = centralizer.projectNormal(normal);
 	const radius = circle.radius/projection.radianScale*centralizer.height;
-	drawSpot(x, y, 'rgba(255, 255, 255, 0.5)');
-	ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)';
+	drawSpot(x, y, circleColor);
+	ctx.strokeStyle = circleColor;
 	ctx.beginPath();
 	ctx.arc(x, y, radius, 0, Math.PI*2);
 	ctx.stroke();
@@ -52,7 +54,7 @@ const drawSearcher = (searcher) => {
 	const space = 1.5;
 	const len = 3;
 	const [ x, y ] = centralizer.projectNormal(searcher.normal);
-	ctx.strokeStyle = '#fc2';
+	ctx.strokeStyle = searcher.test ? '#f00' : searcherColor;
 	ctx.beginPath();
 	ctx.arc(x, y, space + len/3, 0, Math.PI*2);
 	ctx.moveTo(x - space - len, y);
@@ -84,11 +86,61 @@ const render = async () => {
 const updateCircles = () => {
 	const lines = circlesInput.value.split(/\n/).filter(line => line.trim());
 	circles = lines.map(parseCircle);
-	clearSearchers();
 };
 
 const addSearcher = (searcher) => {
 	searchers.push(searcher);
+};
+
+const iterateSearchers = () => {
+	for (let searcher of searchers) {
+		let { active } = searcher;
+		if (active) searcher.tryImprovements();
+		if (active && !searcher.active) {
+			console.log('done', searcher.iterations);
+		}
+	}
+};
+
+const forceFind = () => {
+	const { length } = searchers;
+	for (let i=0; i<1500; +i) {
+		let active = 0;
+		for (let j=0; j<length; ++j) {
+			const searcher = searchers[j];
+			if (searcher.active === false) continue;
+			searcher.tryImprovements();
+			active |= searcher.active;
+		}
+		if (!active) break;
+	}
+};
+
+const mergeDuplicatedResults = () => {
+	const minDist = 1e-6;
+	const result = searchers.slice(0, 1);
+	for (let i=1; i<searchers.length; ++i) {
+		let add = true;
+		const a = searchers[i];
+		for (let j=0; j<result.length; ++j) {
+			const b = searchers[j];
+			const dist = a.distanceTo(b);
+			if (dist < minDist) {
+				add = false;
+				break;
+			}
+		}
+		if (add) {
+			result.push(a);
+		}
+	}
+	searchers.length = 0;
+	searchers.push(...result);
+};
+
+const runSearchers = () => {
+	forceFind();
+	mergeDuplicatedResults();
 };
 
 const clearSearchers = () => {
@@ -103,28 +155,40 @@ const updateSearchersOutput = () => {
 		text += stringifyAngles(lat, 'N', 'S');
 		text += ', ' + stringifyAngles(lon, 'E', 'W') + '\n';
 	});
-	if (text !== searchersOutput.value) {
-		searchersOutput.value = text;
+	if (text !== resultOutput.value) {
+		resultOutput.value = text;
 	}
 };
 
+const resetSearchers = () => {
+	clearSearchers();
+	const nDivs = 4;
+	for (let i=0; i<nDivs; ++i) {
+		const x = (i + 0.5)/nDivs;
+		for (let j=0; j<nDivs; ++j) {
+			const y = (j + 0.5)/nDivs;
+			addSearcher(new Searcher({
+				projection,
+				circles,
+				normal: [ x, y ],
+			}));
+		}
+	}
+	forceFind();
+	mergeDuplicatedResults();
+	updateSearchersOutput();
+};
+
 circlesInput.value = `
-5°10.1'N, 150°27.5'W, 64°50.1'
-89°21.5'N, 138°56.1'E, 44°25'
-19°3.9'N, 52°4.8'W, 44°19'
+16°22.9'S, 55°53.8'W, 65°59.8'
+56°39.6'N, 7°38.3'W, 50°28.6'
+8°55.6'N, 80°25.7'W, 34°30'
 `.split('\n').filter(line => line.trim()).join('\n');
 
 updateCircles();
+resetSearchers();
 adjustCanvasSize();
 render();
-
-setInterval(() => {
-	searchers.forEach((searcher) => {
-		searcher.tryImprovements();
-	});
-	updateSearchersOutput();
-	render();
-}, 0);
 
 window.addEventListener('resize', () => {
 	adjustCanvasSize();
@@ -136,22 +200,15 @@ canvas.addEventListener('mousedown', (e) => {
 	e.stopPropagation();
 });
 
-canvas.addEventListener('dblclick', (e) => {
-	const x = e.offsetX;
-	const y = e.offsetY;
-	const normal = centralizer.getNormal([ x, y ]);
-	const searcher = new Searcher({ projection, circles, normal });
-	addSearcher(searcher);
-	render();
-});
-
 mapSelect.addEventListener('input', () => {
 	const index = mapSelect.value;
 	projection = projections[index];
-	clearSearchers();
+	resetSearchers();
 	render();
 });
 
 circlesInput.addEventListener('input', () => {
 	updateCircles();
+	resetSearchers();
+	render();
 });
